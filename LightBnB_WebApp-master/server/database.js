643aae74ea2e7ed3ab7abeb8caecbc9cfb77a6ae
kyl graym <kylemaxgray@gmail.com>
2021-09-16 19:32:26 +0000

    impliment getUserWithId conn to db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -55,7 +55,17 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
  const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1;`, [`${id}`])
+    .then((result) => {
+      if(!result.rows){
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((error) => {
+      return error.message;
+    })
 }
 exports.getUserWithId = getUserWithId;
 
