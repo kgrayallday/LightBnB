4e26919debed37d0067f3dfebf02c6e21a927ac9
kyl graym <kylemaxgray@gmail.com>
2021-09-16 19:24:06 +0000

    fix getUserWithEmail to call bd

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 /// Users
 
 /**
@@ -8,17 +15,37 @@ const users = require('./json/users.json');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+// - - - OLD CODE BEFORE CONVERSION FOR REFERENCE - - - 
+//
+// const getUserWithEmail = function(email) {
+//   let user;
+//   for (const userId in users) {
+//     user = users[userId];
+//     if (user.email.toLowerCase() === email.toLowerCase()) {
+//       break;
+//     } else {
+//       user = null;
+//     }
+//   }
+//   return Promise.resolve(user);
+// }
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+    .query(`SELECT * FROM users WHERE email LIKE $1;`, [`%${email}%`]) // USE LIKE because emails are case insensitive
+    .then((result) => {
+      console.log('result.row >>> ', result.rows);
+      console.log('result.row[0] >>> ', result.rows[0]);
+      if (!result.rows){
+        return null
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return err.message;
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -27,7 +54,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+ const getUserWithId = function(id) {
   return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
@@ -66,13 +93,17 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+
+ const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((error) => {
+      return error.message;
+    });
+};
 exports.getAllProperties = getAllProperties;
 
 
